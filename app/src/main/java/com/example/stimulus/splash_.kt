bd3fec2e7b68522d526b840a4c5043220f2b7a36
Mohd Shaquib Khan <mohammadshaquib4@gmail.com>
2021-08-17 19:32:15 +0530

    splash screen image

---
+++ app/src/main/java/com/example/stimulus/splash_.kt
@@ -0,0 +1,11 @@
+package com.example.stimulus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class splash_ : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_)
+    }
+}
